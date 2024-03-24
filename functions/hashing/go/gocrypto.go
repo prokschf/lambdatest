0561aa5e5eb7c0c0e13f7125900987b266b1de19
prokschf <proksch.florian@googlemail.com>
2024-03-24 09:53:39 +0100

    m

--- functions/hashing/go/gocrypto.go
+++ functions/hashing/go/gocrypto.go
@@ -1,79 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"crypto/rand"
-	"fmt"
-	"image"
-	"image/draw"
-	"image/jpeg"
-	"net/http"
-	"os"
+	"crypto/sha256"
+	"encoding/hex"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func HandleRequest(ctx context.Context, event map[string][]string) (string, error) {
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		return "", fmt.Errorf("loading AWS config: %w", err)
-	}
-	s3Client := s3.NewFromConfig(cfg)
-
-	outputBucket := os.Getenv("OUTPUT_BUCKET")
-	for _, url := range event["urls"] {
-		resp, err := http.Get(url)
-		if err != nil {
-			return "", fmt.Errorf("downloading image: %w", err)
-		}
-		defer resp.Body.Close()
-
-		img, _, err := image.Decode(resp.Body)
-		if err != nil {
-			return "", fmt.Errorf("decoding image: %w", err)
-		}
-
-		// Example resize, assuming img is square for simplicity
-		// For actual resizing, consider third-party libraries like github.com/nfnt/resize
-		resizedImg := image.NewRGBA(image.Rect(0, 0, 100, 100))
-		draw.Draw(resizedImg, resizedImg.Bounds(), img, img.Bounds().Min, draw.Src)
-
-		buf := new(bytes.Buffer)
-		err = jpeg.Encode(buf, resizedImg, nil)
-		if err != nil {
-			return "", fmt.Errorf("encoding image: %w", err)
-		}
-
-		// Generate random file name
-		fileName := generateRandomString(10) + ".jpg"
-		_, err = s3Client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket:      aws.String(outputBucket),
-			Key:         aws.String(fileName),
-			Body:        bytes.NewReader(buf.Bytes()),
-			ContentType: aws.String("image/jpeg"),
-		})
-		if err != nil {
-			return "", fmt.Errorf("uploading image to S3: %w", err)
-		}
-	}
-
-	return "Image processing completed.", nil
+type MyEvent struct {
+	InputString string `json:"inputString"`
 }
 
-func generateRandomString(n int) string {
-	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	bytes := make([]byte, n)
-	if _, err := rand.Read(bytes); err != nil {
-		panic(err) // rand.Read should never fail
-	}
-	for i, b := range bytes {
-		bytes[i] = letters[b%byte(len(letters))]
-	}
-	return string(bytes)
+type MyResponse struct {
+	SHA256Hash string `json:"sha256Hash"`
+}
+
+func HandleRequest(ctx context.Context, event MyEvent) (MyResponse, error) {
+	// Calculate SHA-256 hash of the input string
+	h := sha256.New()
+	h.Write([]byte(event.InputString))
+	sha256Hash := hex.EncodeToString(h.Sum(nil))
+
+	return MyResponse{SHA256Hash: sha256Hash}, nil
 }
 
 func main() {
